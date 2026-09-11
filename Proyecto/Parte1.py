329b462f315cd7b22854b6e147fa0855012034f1
Javier Farias <jafarias7@uc.cl>
2026-09-11 09:37:26 -0300

    aa

--- Proyecto/Parte1.py
+++ Proyecto/Parte1.py
@@ -1,6 +1,15 @@
-#hola 
+import pandas as pd
+import numpy as np
+import simpy
+from scipy.stats import expon, lognorm, kstest, kruskal, binom, chi2, gamma, poisson
+import matplotlib.pyplot as plt
 
-#vice quiero hablar contigo
-#tengo control klooooo
 
-#pero vice volvamos a ser amigos te extraño
+#se leen los archivos y se agrega el separador junto con el decimal para "adaptar" los datos al lenguaje de py, ya que venían con el decimal "," por defecto
+log_operacional = pd.read_csv("log_operacional_historico.csv", sep=";", decimal = ",")
+log_reparaciones = pd.read_csv("log_reparaciones_historico.csv", sep=";", decimal = ",")
+
+
+##################### PARTE 1a ##########################
+
+print(log_operacional["event_type"].value_counts())
